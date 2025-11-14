3d32bf3c9c67c63a4ff9461de32abf729caa911c
khoo_shi <Khushdeepk1706@gmail.com>
2025-11-14 18:29:13 -0500

    Add Smoothie class skeleton

---
+++ script.js
@@ -0,0 +1,8 @@
+class Smoothie {
+    constructor(name, size, ingredients, sweetness) {
+        this.name = name;
+        this.size = size;
+        this.ingredients = ingredients;
+        this.sweetness = sweetness;
+    }
+}
