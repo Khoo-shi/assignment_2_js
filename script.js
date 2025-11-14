66c2b0f38c33845726a3b0eed4b9cabf5e03d7cf
khoo_shi <Khushdeepk1706@gmail.com>
2025-11-14 18:31:09 -0500

    Added describe() method to Smoothie class

--- script.js
+++ script.js
@@ -5,4 +5,14 @@ class Smoothie {
         this.ingredients = ingredients;
         this.sweetness = sweetness;
     }
+
+    describe() {
+    return `
+        <strong>Name:</strong> ${this.name} <br>
+        <strong>Size:</strong> ${this.size} <br>
+        <strong>Ingredients:</strong> ${this.ingredients.join(", ")} <br>
+        <strong>Sweetness:</strong> ${this.sweetness}
+    `;
+}
+
 }
